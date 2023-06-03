1a1c95f67356c2859b30b1793ba30c8ec305ae8a
AlanCienega <craulalan115@gmail.com>
2023-06-03 14:50:48 -0600

    event listener on input

--- html/event_listener/index.js
+++ html/event_listener/index.js
@@ -1,15 +1,26 @@
 const header = document.querySelector("header");
 const counter_tag = document.querySelector("span");
 const button = document.querySelector("button");
+const input = document.querySelector("input");
+const dynamic_text = document.getElementById("dynamic_text");
+
 let counter = 0;
 header.textContent = "Event Listener";
 
 /**
  * addEventListener acepta como primer parametro el nombre del evento que vamos a escuchar
  * en este caso el click, como segundo parametro es una funcion que recibe por defecto un evento
- * este puede ser omitido pero esta vez lo he puesto para ver lo que obtengo en consola
  */
-button.addEventListener("click", (event) => {
+button.addEventListener("click", () => {
   counter++;
   counter_tag.textContent = counter;
+});
+
+input.addEventListener("keyup", (event) => {
+  /**
+   * alguna solucion que podrias pensar es obtener primero el valor de input desde el queryselector
+   * dynamic_text.textContent = input.value
+   * pero ya el evento nos da el valor del input asi que no necesitamos traerlo de nuevo
+   */
+  dynamic_text.textContent = event.target.value;
 });
